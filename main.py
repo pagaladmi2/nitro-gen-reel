12ebb53dd012888328b77ab727ad3f34f26292e2
pagaladmi <86248778+pagaladmi2@users.noreply.github.com>
2022-02-06 09:13:48 +0530

    Create main.py
    t

---
+++ main.py
@@ -0,0 +1,7 @@
+import string
+import random
+import os
+os.system('holy shit why would you use this')
+for i in range(69):
+  obesity = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
+  print(f" SOCIAL CREDITS GENERATED https://discord.gift/{obesity}")
